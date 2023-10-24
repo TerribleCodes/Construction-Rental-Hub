d3b775ea9e8886cf48485240240498e4aea6593c
TerribleCodes <madawadesilva9998@gmail.com>
2023-10-24 14:13:55 +0530

    changed script file locations

--- update-product.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-  UPDATE hr.products
-  SET product_name = :product_name,
-      product_price = :product_price,
-      product_stock = :product_stock
-  WHERE product_id = :product_id;
-
-  COMMIT;
-END;
